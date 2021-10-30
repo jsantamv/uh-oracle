140ad8e93e7179f74f23ac169d3cc97ccef1b72c
juan.santamaria <jsantamv@live.com>
2021-10-30 13:47:35 -0600

    Actualizacion de los archivos.  Control Files

---
+++ control-file/controlfile.sql
@@ -0,0 +1,30 @@
+
+STARTUP NOMOUNT;
+SHUTDOWN IMMEDIATE;
+
+select * from DBA_database
+ 
+show parameter control  --Mejor utilizar este parametro para ver mis archivos
+SELECT * FROM V$PARAMETER WHERE NAME = 'control_files'; --ojo tener cuidado con las mayusculas. 
+   
+select * from V$LOGFILE;
+select * from V$DATAFILE; 
+
+
+CREATE CONTROLFILE
+   SET DATABASE prod
+   LOGFILE GROUP 1 ('C:\ORACLEXE\APP\ORACLE\FAST_RECOVERY_AREA\XE\ONLINELOG\O1_MF_2_JNF4DG2X_.LOG',                  
+           GROUP 2 ('C:\ORACLEXE\APP\ORACLE\FAST_RECOVERY_AREA\XE\ONLINELOG\O1_MF_1_JNF4DFX9_.LOG',                   
+   NORESETLOGS
+   DATAFILE '/u01/oracle/prod/system01.dbf' SIZE 3M,
+            '/u01/oracle/prod/rbs01.dbs' SIZE 5M,
+            '/u01/oracle/prod/users01.dbs' SIZE 5M,
+            '/u01/oracle/prod/temp01.dbs' SIZE 5M
+   MAXLOGFILES 50
+   MAXLOGMEMBERS 3
+   MAXLOGHISTORY 400
+   MAXDATAFILES 200
+   MAXINSTANCES 6
+   ARCHIVELOG;
+   
+   
