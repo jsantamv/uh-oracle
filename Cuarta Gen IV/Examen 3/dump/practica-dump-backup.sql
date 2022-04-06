00edabfabf314fe1a64a4cdff146d9208448a23c
juan.santamaria <jsantamv@live.com>
2022-04-05 19:31:29 -0600

    actualizacion de fuentes y Clase tercer examen

---
+++ Cuarta Gen IV/Examen 3/dump/practica-dump-backup.sql
@@ -0,0 +1,9 @@
+
+-- 1. Realizar un script que permita generar todos los dias un backup del schema OE y HR
+-- Pero del Schema HR a la tabla Empleados solo los empleados que ganan mas de 2000.
+-- El nombre del archivo export debara de crearse de manera automatica yyyy_MM_dd.dmp
+-- utilizar un archivo .par para generar o ejecutarlo desde un .bat
+
+
+
+
